425a3173b6fb380c9f505f69633b7d2c084763e1
sup <sup@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-01-23 14:06:50 +0000

    Master page prepare

--- Tripi/TwoColumnsWithFooter.Master.designer.cs
+++ Tripi/TwoColumnsWithFooter.Master.designer.cs
@@ -12,7 +12,7 @@ namespace Tripi
 {
 
 
-    public partial class _Default
+    public partial class TwoColumnsWithFooter
     {
 
         /// <summary>
@@ -23,5 +23,14 @@ namespace Tripi
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
