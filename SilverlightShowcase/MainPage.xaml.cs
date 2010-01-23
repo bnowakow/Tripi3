20fe53125ad99307030d319d7832dbd7c8980eca
sup <sup@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-01-23 23:16:23 +0000

    Calling WebService from silverlight

--- SilverlightShowcase/MainPage.xaml.cs
+++ SilverlightShowcase/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace SilverlightShowcase
 {
@@ -17,6 +18,21 @@ namespace SilverlightShowcase
         public MainPage()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            SampleWeatherService.GlobalWeatherSoapClient sws = new SampleWeatherService.GlobalWeatherSoapClient();
+            sws.GetWeatherCompleted += new EventHandler<SilverlightShowcase.SampleWeatherService.GetWeatherCompletedEventArgs>(sws_GetWeatherCompleted);
+            sws.GetWeatherAsync("Gdansk-Rebiechowo", "Poland");
+        }
+
+        void sws_GetWeatherCompleted(object sender, SilverlightShowcase.SampleWeatherService.GetWeatherCompletedEventArgs e)
+        {
+            txtName.Text = e.Result.ToString();
+          
+
         }
     }
 }
