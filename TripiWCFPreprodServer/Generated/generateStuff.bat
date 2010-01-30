8c43b0f6973fd200630cfec9f199c4294c4e15c0
tanstaafl <tanstaafl@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-01-30 18:52:50 +0000

    For ease of generation.

--- TripiWCFPreprodServer/Generated/generateStuff.bat
+++ TripiWCFPreprodServer/Generated/generateStuff.bat
@@ -1,2 +1,5 @@
-﻿svcutil TripiWCFService.dll
-svcutil /namespace:*,TripiWCF.ClientMockup.Proxy tempuri.org.wsdl *.xsd /out:ProxyTripiWCFService.cs
+rem svcutil TripiWCFService.dll
+rem svcutil /namespace:*,TripiWCF.ClientMockup.Proxy tempuri.org.wsdl *.xsd /out:ProxyTripiWCFService.cs
+netcfsvcutil.exe tempuri.org.wsdl tempuri.org.xsd TripiWCF.Service.xsd schemas.microsoft.com.2003.10.Serialization.xsd schemas.microsoft.com.Message.xsd
+move tempuri.org.cs .\ForMobile\
+move CFClientBase.cs .\ForMobile\
