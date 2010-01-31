c01b40c64a9363df1ce845f981d210217604c42c
joaha <joaha@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-01-31 20:57:14 +0000

    Poprawki drobne i drobniejsze

--- Tripi/Login.aspx.cs
+++ Tripi/Login.aspx.cs
@@ -10,7 +10,6 @@ public partial class Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
     }
 
     protected void AutenticateUser(object sender, AuthenticateEventArgs e)
