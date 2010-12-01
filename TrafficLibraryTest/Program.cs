14b30ba3da3672efd5f0c5787116c52e3cd4ae02
joaha <joannaruth1@gmail.com>
2010-12-01 20:09:52 +0000

    Test project added for TrafficLibrary

---
+++ TrafficLibraryTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrafficLibrary;
+
+namespace TrafficLibraryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estimation estimation = new Estimation();
+            EstimationPoint point = estimation.CalculateEstimationPoint(18.59360, 54.38022, DateTime.Parse("2010-11-22 16:38:35"));
+
+        }
+    }
+}
