d6f1d2708db9864b3877415070f1c7f5e7d8fd14
joaha <joannaruth1@gmail.com>
2010-12-01 18:44:17 +0000

    TrafficLibrary first classes

---
+++ TrafficLibrary/Estimation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficLibrary
+{
+    public class Estimation
+    {
+        public EstimationPoint CalculateEstimationPoint(double latitude, double longitude)
+        {
+            return new EstimationPoint(DateTime.Now, 0.0, 0.0, 100);
+        }
+    }
+}
---
+++ TrafficLibrary/EstimationPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace TrafficLibrary
+{
+    [DataContract]
+    public class EstimationPoint
+    {
+        protected DateTime date;
+        protected double latitude;
+        protected double longitude;
+        protected double speed;
+
+        public EstimationPoint(DateTime date, double latitude, double longitude, double speed)
+        {
+            this.date = date;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.speed = speed;
+        }
+
+        [DataMember]
+        public DateTime Date
+        {
+            get { return date; }
+            private set { date = value; }
+        }
+
+        [DataMember]
+        public double Latitude
+        {
+            get { return latitude; }
+            private set { latitude = value; }
+        }
+
+        [DataMember]
+        public double Longitude
+        {
+            get { return longitude; }
+            private set { longitude = value; }
+        }
+
+        [DataMember]
+        public double Speed
+        {
+            get { return speed; }
+            private set { speed = value; }
+        } 
+    }
+}
