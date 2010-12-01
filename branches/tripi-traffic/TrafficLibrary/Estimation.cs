cbd88f7374364ff280ee852f06d15750795651c1
joaha <joaha@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-12-01 20:22:25 +0000

    added strategy pattern to TrafficLibrary

--- branches/tripi-traffic/TrafficLibrary/Estimation.cs
+++ branches/tripi-traffic/TrafficLibrary/Estimation.cs
@@ -8,17 +8,18 @@ namespace TrafficLibrary
     public class Estimation
     {
         private readonly double distanceTreshold = 0.0001;
+        private EstimationStrategy strategy = null;
+
+        public Estimation(EstimationStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
 
         public EstimationPoint CalculateEstimationPoint(double latitude, double longitude, DateTime date)
         {
             List<RawPoint> rawPoints = LoadRawPoints();
-
-            var pointsSortedByDistance = rawPoints.OrderBy(point => point.GetDistanceFromPoint(longitude, latitude)).Take(5);
-            double avgSpeed = pointsSortedByDistance.Average(point => point.Speed);
-            double avgLatitude = pointsSortedByDistance.Average(point => point.Latitude);
-            double avgLongitude = pointsSortedByDistance.Average(point => point.Longitude);
-
-            return new EstimationPoint(date, avgLatitude, avgLongitude, avgSpeed);
+            EstimationPoint result = strategy.RunEstimation(latitude, longitude, date, rawPoints);
+            return result;
         }
 
         private List<RawPoint> LoadRawPoints()
---
+++ branches/tripi-traffic/TrafficLibrary/EstimationStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficLibrary
+{
+    public interface EstimationStrategy
+    {
+        EstimationPoint RunEstimation(double latitude, double longitude, DateTime date, List<RawPoint> rawPoints);    
+    }
+}
