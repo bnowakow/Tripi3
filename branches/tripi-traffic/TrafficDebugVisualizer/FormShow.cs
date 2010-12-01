ac9c9e1788d2f5d3371462736e1801f02501cc4c
tanstaafl <tanstaafl@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-12-01 21:18:27 +0000

    Made TrafficDebugVisualizer/FormShow.cs look better.

--- branches/tripi-traffic/TrafficDebugVisualizer/FormShow.cs
+++ branches/tripi-traffic/TrafficDebugVisualizer/FormShow.cs
@@ -20,10 +20,11 @@ namespace TrafficDebugVisualizer
             groupBoxPiniatas.Paint += new PaintEventHandler(groupBoxPiniatas_Paint);
         }
 
+        private Pen WhitePen = new Pen(Color.White, 2);
         void groupBoxPiniatas_Paint(object sender, PaintEventArgs e)
         {
-            if (pointGetterRaw.ShouldBeDrawn) DrawPoints(e.Graphics, pointGetterRaw.PointBuffer);
-            if (pointGetterEstimated.ShouldBeDrawn) DrawPoints(e.Graphics, pointGetterEstimated.PointBuffer);
+            if (pointGetterRaw.ShouldBeDrawn) DrawPoints(e.Graphics, pointGetterRaw.PointBuffer, Pens.Black);
+            if (pointGetterEstimated.ShouldBeDrawn) DrawPoints(e.Graphics, pointGetterEstimated.PointBuffer, WhitePen);
         }
 
         private void FormShow_Load(object sender, EventArgs e)
@@ -39,17 +40,24 @@ namespace TrafficDebugVisualizer
             pointGetterEstimated.PointsWanted += new Action<List<EstimationPoint>, bool>(pointGetterAny_PointsWanted);
         }
 
-        void pointGetterAny_PointsWanted(List<EstimationPoint> arg1, bool refresh)
+        void pointGetterAny_PointsWanted(List<EstimationPoint> points, bool refresh)
         {
             groupBoxPiniatas.Invalidate();
         }
 
-        void DrawPoints(Graphics canvas, List<EstimationPoint> points)
+        void DrawPoints(Graphics canvas, List<EstimationPoint> points, Pen circle)
         {
             foreach (EstimationPoint ep in points)
             {
-                canvas.FillEllipse(Brushes.Red, ConvertLat(ep.Latitude) + 50, ConvertLon(ep.Longitude) + 50, 10, 10);
+                canvas.FillEllipse(new SolidBrush(Color.FromArgb(Clr(ep.Speed), 0, 255 - Clr(ep.Speed))), ConvertLat(ep.Latitude), ConvertLon(ep.Longitude), 10, 10);
+                canvas.DrawEllipse(circle, ConvertLat(ep.Latitude), ConvertLon(ep.Longitude), 10, 10);
             }
+        }
+
+        private int Clr(double speed)
+        {
+            int clr = 5 * (int)speed;
+            return clr > 255 ? 255 : clr;
         }
 
         private List<EstimationPoint> GetRawPoints(string filename)
@@ -62,7 +70,7 @@ namespace TrafficDebugVisualizer
             string[] splat = latLonTime.Split(' ');
             Estimation ester = new Estimation(new RadialEstimationStrategy());
 
-            return new List<EstimationPoint>() { ester.CalculateEstimationPoint(double.Parse(splat[0]), double.Parse(splat[1]), DateTime.Now) };
+            return new List<EstimationPoint>() { ester.CalculateEstimationPoint(double.Parse(splat[0]), double.Parse(splat[1]), DateTime.Parse(splat[2])) };
         }
 
         private void FormShow_Paint(object sender, PaintEventArgs e)
@@ -76,14 +84,15 @@ namespace TrafficDebugVisualizer
             if (pointGetterEstimated.ShouldBeDrawn) DrawPoints(pg, pointGetterEstimated.PointBuffer);*/
         }
 
+        private const float FakeScale = 6000;
         private float ConvertLat(double lat)
         {
-            return (float)((lat - 54.36) * 2000);
+            return (float)((lat - 54.36) * FakeScale);
         }
 
         private float ConvertLon(double lon)
         {
-            return (float)((lon - 18.5) * 2000);
+            return (float)((lon - 18.5) * FakeScale);
         }
     }
 }
