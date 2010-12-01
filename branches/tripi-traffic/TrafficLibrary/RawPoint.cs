861c0651d09c462fe4ee80f7ed25fa31264eb4b6
tanstaafl <tanstaafl@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-12-01 19:02:19 +0000

    Parser created.

--- branches/tripi-traffic/TrafficLibrary/RawPoint.cs
+++ branches/tripi-traffic/TrafficLibrary/RawPoint.cs
@@ -3,22 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Text.RegularExpressions;
+
 namespace TrafficLibrary
 {
     public class RawPoint : EstimationPoint
     {
-        protected String id; 
+        protected String id;
 
-        public RawPoint(String id, DateTime date, double latitude, double longitude, double speed) 
+        public RawPoint(String id, DateTime date, double latitude, double longitude, double speed)
             : base(date, latitude, longitude, speed)
         {
             this.id = id;
+        }
+
+        public RawPoint(string[] gpsInputFragments)
+            : this(gpsInputFragments[1], DateTime.Parse(gpsInputFragments[2]), double.Parse(gpsInputFragments[3]), double.Parse(gpsInputFragments[4]), double.Parse(gpsInputFragments[8]))
+        {
+        }
+
+        public RawPoint(string rawGpsInput)
+            : this(Regex.Matches(rawGpsInput, @"([0-9]+)@([^@]+)@([0-9\.]+)@([0-9\.]+)@([NS] [0-9\.]+)&deg;@([EW] [0-9\.]+)&deg;@([0-9]+) m@([0-9]+) km/h@([0-9]+)&deg;")
+                .Cast<Match>().Select(m => m.Value).ToArray())
+        {
         }
 
         public String Id
         {
             get { return id; }
             private set { id = value; }
+        }
+
+        public static RawPoint[] Parsed(string filename)
+        {
+            return System.IO.File.ReadAllText(filename).Split('|').Select(entry => new RawPoint(entry)).ToArray();
         }
     }
 }
---
+++ branches/tripi-traffic/TrafficParser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace TrafficParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Enter directory (<Enter> assumes ""..\..\..\Install\iPhone Gps data\raw"")");
+            string folder = Console.ReadLine();
+            if (folder.Length == 0) folder = @"..\..\..\Install\iPhone Gps data\raw";
+
+            foreach (string filename in Directory.GetFiles(folder, "*.txt")) Console.WriteLine(filename);
+            Console.ReadLine();
+        }
+    }
+}
