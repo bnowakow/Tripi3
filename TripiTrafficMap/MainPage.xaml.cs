b3df5e707d3d0eca0b2d001bb68835451a48423d
Bartek Nowakowski <bnowakow@users.noreply.github.com>
2010-12-02 02:16:24 +0000

    Map prototype 0.1

---
+++ TripiTrafficMap/MainPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TripiTrafficMap.Util.Interface;
+using TripiTrafficMap.Util;
+using Microsoft.Maps.MapControl;
+using System.Xml;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace TripiTrafficMap
+{
+    public partial class MainPage : UserControl
+    {
+        protected IMapStartPositionAdjuster mapStartPositionAdjuster;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Map.Width = Application.Current.Host.Content.ActualWidth;
+            mapStartPositionAdjuster = new MapStartPositionAdjuster(Map);
+            drawTestPolyline();
+        }
+
+        protected IList<Location> getSamplePositionNodeList()
+        {
+            IList<Location> points = new List<Location>();
+            for (int i = 8; i < 9; i++)
+            {
+                XmlReader reader = XmlReader.Create(@"TmpXml/00" + i + ".xml");
+                while (true)
+                {
+                    Location point = new Location();
+                    reader.ReadToFollowing("Latitude");
+                    reader.Read();
+                    if (reader.Value == "")
+                    {
+                        break;
+                    }
+                    point.Latitude = Double.Parse(reader.Value, System.Globalization.NumberStyles.Any, new NumberFormatInfo());
+                    reader.ReadToFollowing("Longitude");
+                    reader.Read();
+                    point.Longitude = Double.Parse(reader.Value, System.Globalization.NumberStyles.Any, new NumberFormatInfo());
+                    reader.ReadToFollowing("Speed");
+                    reader.Read();
+                    point.Altitude = Double.Parse(reader.Value, System.Globalization.NumberStyles.Any, new NumberFormatInfo());
+                    points.Add(point);
+                }
+                reader.Close();
+                break;
+            }
+            return points;
+        }
+
+        private void drawTestPolyline()
+        {
+            IList<Location> pointList = this.getSamplePositionNodeList();
+            
+            Location prevLocation = null;
+            foreach (Location location in pointList)
+            {
+                if (prevLocation != null)
+                {
+                    LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
+                    GradientStop gradientStart = new GradientStop();
+                    GradientStop gradientStop = new GradientStop();
+
+                    gradientStart.Offset = 0.0;
+                    gradientStop.Offset = 0.5;
+                    gradientStart.Color = Color.FromArgb(255, (byte)Math.Round(80 /prevLocation.Altitude * 255, 0), (byte)Math.Round(prevLocation.Altitude /80 * 255, 0), 0);
+                    gradientStop.Color = Color.FromArgb(255, (byte)Math.Round(80 / location.Altitude * 255, 0), (byte)Math.Round(location.Altitude / 80 * 255, 0), 0);
+
+                    linearGradientBrush.StartPoint = new Point((180.0 + prevLocation.Longitude) / (Map.ViewportSize.Width /360.0), (90.0-prevLocation.Latitude) / (Map.ViewportSize.Height / 180.0));
+                    linearGradientBrush.EndPoint = new Point((180.0 + location.Longitude) / (Map.ViewportSize.Width / 360.0), (90.0 - location.Latitude) / (Map.ViewportSize.Height / 180.0));
+
+                    linearGradientBrush.StartPoint = new Point((180.0 + prevLocation.Longitude) / (360.0), (90.0 - prevLocation.Latitude) / (180.0));
+                    linearGradientBrush.EndPoint = new Point((180.0 + location.Longitude) / (360.0), (90.0 - location.Latitude) / (180.0));
+
+                    linearGradientBrush.StartPoint = new Point(0, 0);
+                    linearGradientBrush.EndPoint = new Point(1,0);
+
+                    linearGradientBrush.GradientStops = new GradientStopCollection();
+
+                    linearGradientBrush.GradientStops.Add(gradientStart);
+                    linearGradientBrush.GradientStops.Add(gradientStop);
+
+                    MapPolyline mapPolyline = new MapPolyline();
+                    mapPolyline.Stroke = linearGradientBrush;
+                    mapPolyline.StrokeThickness = 5;
+                    mapPolyline.Opacity = 0.9;
+                    mapPolyline.Locations = new LocationCollection();
+                    mapPolyline.Locations.Add(prevLocation);
+                    mapPolyline.Locations.Add(location);
+                    Map.Children.Add(mapPolyline);
+                    //break;
+                }
+                prevLocation = location;
+            }
+            
+            mapStartPositionAdjuster.SetMapCenterPointAndZoomLevel(pointList);
+        }
+
+    }
+}
---
+++ TripiTrafficMap/Util/MapStartPositionAdjuster.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using Microsoft.Maps.MapControl;
+using TripiTrafficMap.Util.Interface;
+
+namespace TripiTrafficMap.Util
+{
+    public class MapStartPositionAdjuster : IMapStartPositionAdjuster
+    {
+        protected Map map;
+        protected int pixelBorder;
+
+        public MapStartPositionAdjuster(Map map, int pixelBorder = 10)
+        {
+            this.map = map;
+            this.pixelBorder = pixelBorder;
+        }
+
+        public void SetMapCenterPointAndZoomLevel(IList<Location> locations)
+        {
+            this.SetMapCenterPointAndZoomLevel(locations, this.map, this.pixelBorder);
+        }
+
+        /// <summary>
+        /// Adjusts map zoom level and map center to show all defined points at the same time preserving constant margin around them.
+        /// </summary>
+        /// <param name="locations">List of points which should be visible at the same time</param>
+        /// <param name="map">Map which will be customized</param>
+        /// <param name="pixelBorder">Margin in pixels around points</param>
+        public void SetMapCenterPointAndZoomLevel(IList<Location> locations, Map map, int pixelBorder)
+        {
+            Location center = new Location();
+            double zoomLevel = 0.0;
+            
+            double maxLat = -85.0;
+            double minLat = 85.0;
+            double maxLon = -180.0;
+            double minLon = 180.0;
+
+           
+
+            //calculate bounding rectangle
+            for (int i = 0; i < locations.Count; i++)
+            {
+                if (locations[i].Latitude > maxLat)
+                {
+                    maxLat = locations[i].Latitude;
+                }
+                
+                if (locations[i].Latitude < minLat)
+                {
+                    minLat = locations[i].Latitude;
+                }
+
+                if (locations[i].Longitude > maxLon)
+                {
+                    maxLon = locations[i].Longitude;
+                }
+
+                if (locations[i].Longitude < minLon)
+                {
+                    minLon = locations[i].Longitude;
+                }
+            }
+            
+            center.Latitude = (maxLat + minLat) / 2.0;
+            center.Longitude = (maxLon + minLon) / 2.0;
+            
+            double zoom1 = 0.0, zoom2 = 0.0;
+
+            //Determine the best zoom level based on the map scale and bounding coordinate information
+            if (maxLon != minLon && maxLat != minLat)
+            {
+                //best zoom level based on map width
+                zoom1 = Math.Log(360.0 / 256.0 * (map.ViewportSize.Width - 2.0 * pixelBorder) / (maxLon - minLon)) / Math.Log(2.0);
+                //best zoom level based on map height
+                zoom2 = Math.Log(180.0 / 256.0 * (map.ViewportSize.Height - 2.0 * pixelBorder) / (maxLat - minLat)) / Math.Log(2.0);
+            }
+            
+            //use the most zoomed out of the two zoom levels
+            zoomLevel = (zoom1 < zoom2) ? zoom1 : zoom2;
+            
+            map.Center = center;
+            map.ZoomLevel = zoomLevel;
+        }
+    }
+}
