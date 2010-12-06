2e291fbf35af4211e3f552b18b4774a4e64c735b
Bartek Nowakowski <bnowakow@users.noreply.github.com>
2010-12-06 14:17:22 +0000

    Not even close to finished and working ;x Part 2.

--- SilverlightColorPicker/ColorSlider.xaml.cs
+++ SilverlightColorPicker/ColorSlider.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
-namespace ColorPicker
+namespace ColorPickerControl
 {
     public partial class ColorSlider : UserControl
     {
@@ -21,14 +21,8 @@ namespace ColorPicker
         //// http://blogs.silverlight.net/blogs/msnow/archive/2008/08/26/silverlight-tip-of-the-day-32-how-to-declare-a-custom-user-control-from-a-xaml-page.aspx
         //http://weblogs.asp.net/scottgu/pages/silverlight-tutorial-part-6-using-user-controls-to-implement-master-detail-scenarios.aspx
         // http://paulyanez.com/interactive/index.php/2009/12/scaling-objects-with-a-slider-control-in-silverlight/
-        private void ClickSelectedMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            //SliderThumb.SetValue(Canvas.LeftProperty, e.);
-        }
 
-        private void SliderThumb_MouseMove(object sender, MouseEventArgs e)
-        {
-            
-        }
+        //http://www.dotnetspark.com/kb/1844-customize-slider-control-silverlight-application.aspx
+        
     }
 }
