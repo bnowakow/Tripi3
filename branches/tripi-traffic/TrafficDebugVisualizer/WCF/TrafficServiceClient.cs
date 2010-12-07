09466851f0edb0fb847c4234d85bd9351d9e56bf
joaha <joaha@399277ac-2437-41f9-8725-ab45cbfe0cf9>
2010-12-07 19:56:37 +0000

    Added methods to partial class TrafficServiceClient.cs ^^

---
+++ branches/tripi-traffic/TrafficDebugVisualizer/WCF/TrafficServiceClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace TrafficDebugVisualizer.WCF
+{
+    partial class TrafficServiceClient
+    {
+        public TrafficServiceClient(Uri ip)
+            : this(new BasicHttpBinding(), new EndpointAddress(ip))
+        {
+        }
+
+        public TrafficLibrary.EstimationPoint GetEstimationPoint(string lat, string lon, string tiem)
+        {
+            EstimationPoint ep = GetEstimationPoint(double.Parse(lat), double.Parse(lon), DateTime.Parse(tiem));
+            return new TrafficLibrary.EstimationPoint(ep.Date, ep.Latitude, ep.Longitude, ep.Speed);
+        }
+    }
+}
